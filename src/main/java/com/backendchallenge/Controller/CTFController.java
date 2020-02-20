39c5dc1c405d386cffddf3a65a4592391173b5ae
Danilo Endava <nicolasdpf@gmail.com>
2020-02-20 11:34:19 -0500

    Method findById added

--- src/main/java/com/backendchallenge/Controller/CTFController.java
+++ src/main/java/com/backendchallenge/Controller/CTFController.java
@@ -1,8 +1,10 @@
 package com.backendchallenge.Controller;
 
 import com.backendchallenge.model.CTF;
+import com.backendchallenge.persistence.CTFRepository;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,18 @@ import java.util.Collection;
 @RestController
 @RequestMapping("CTF/api")
 public class CTFController {
-    private MongoTemplate mongoTemplate;
+    private CTFRepository ctfRepository;
 
-    public CTFController(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
+    public CTFController(CTFRepository ctfRepository) {
+        this.ctfRepository = ctfRepository;
     }
-    @GetMapping("/all")
-    public Collection<CTF> all(){ return this.mongoTemplate.findAll(CTF.class);}
 
+    @GetMapping("/all")
+    public Collection<CTF> all(){ return this.ctfRepository.findAll();}
+
+    @GetMapping("/{id}")
+    public CTF byId(@PathVariable String id){
+        return this.ctfRepository.findById(id).orElse(null);    
+    }
 
 }
