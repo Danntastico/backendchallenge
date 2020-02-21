3702f7aff7d68fb4dc4317a92037ffccdb7f1837
Danilo Endava <nicolasdpf@gmail.com>
2020-02-21 11:33:55 -0500

    Optimized imports

--- src/main/java/com/backendchallenge/model/CTFRepository.java
+++ src/main/java/com/backendchallenge/model/CTFRepository.java
@@ -1,9 +1,6 @@
 package com.backendchallenge.model;
 
-import com.backendchallenge.model.CTF;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.Collection;
 
 public interface CTFRepository extends MongoRepository<CTF, String> {
 }
