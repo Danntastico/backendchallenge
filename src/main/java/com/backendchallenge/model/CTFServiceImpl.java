62d765736f4400a9f1fa7a1e59f21550240d8e38
Danilo Endava <nicolasdpf@gmail.com>
2020-02-21 11:52:03 -0500

    Implemented methods in Service for the first challenge's microservice

--- src/main/java/com/backendchallenge/model/CTFServiceImpl.java
+++ src/main/java/com/backendchallenge/model/CTFServiceImpl.java
@@ -1,20 +1,35 @@
 package com.backendchallenge.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class CTFServiceImpl implements CTFService {
+    @Autowired
+    private CTFRepository ctfRepository;
+
     @Override
     public List<CTF> findAll() {
-        return null;
+        return this.ctfRepository.findAll();
     }
 
     @Override
     public List<Integer> getAllIdAnimes() {
-        return null;
+        List<Integer> id = new ArrayList<>();
+        for(CTF ctf : ctfRepository.findAll()){
+            id.add(ctf.getAnimeId());
+        }
+        return id;
     }
 
     @Override
     public CTF findAnimeById(int id) {
+        for (CTF ctf : ctfRepository.findAll()){
+            if (ctf.getAnimeId() == id){
+                return ctf;
+            }
+        }
         return null;
     }
 }
