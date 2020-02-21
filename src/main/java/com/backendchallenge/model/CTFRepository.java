26d6ef0a065b6e9154d547b9ef5a1a4e713293ee
Danilo Endava <nicolasdpf@gmail.com>
2020-02-21 11:32:32 -0500

    Deleted any implementation from the CTF repository Interface

--- src/main/java/com/backendchallenge/model/CTFRepository.java
+++ src/main/java/com/backendchallenge/model/CTFRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Collection;
 
 public interface CTFRepository extends MongoRepository<CTF, String> {
-    Collection<CTF> findAllByTypeContains(String type);
 }
