b4fdb6b13c8c4e9719764124d235d469126c2f77
Danilo Endava <nicolasdpf@gmail.com>
2020-03-02 18:12:07 -0500

    adding TOPList method to the Controller.

--- src/main/java/com/backendchallenge/controller/CTFController.java
+++ src/main/java/com/backendchallenge/controller/CTFController.java
@@ -27,4 +27,14 @@ public class CTFController {
     public CTF byId(@PathVariable int anime_id){
         return ctfService.findAnimeById(anime_id);
     }
+
+    @GetMapping("GET/anime/top")
+    public List<Integer> topId(@RequestParam(name = "limit", required = false) Integer limit,
+                               @RequestParam(name = "genre", required = false) String genre,
+                               @RequestParam(name = "type", required = false) String type,
+                               @RequestParam(name = "studio", required = false) String studio,
+                               @RequestParam(name = "source", required = false) String source,
+                               @RequestParam(name = "mainCast", required = false) String mainCast){
+        return ctfService.topId(limit, genre, type, studio,source, mainCast);
+    }
 }
